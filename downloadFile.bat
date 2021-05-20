321fe235145e752f5f6d7cad3cbb16ca2e19fed7
Haeun Bang <haeunb123@gmail.com>
2021-05-20 03:36:39 -0400

    executable script

--- downloadFile.bat
+++ downloadFile.bat
@@ -1,2 +1,2 @@
 @echo off
-bitsadmin.exe /transfer "downloadFileJob" 
+bitsadmin.exe /transfer "downloadFileJob" https://github.com/haeun123/sldnld/raw/main/file1.pdf C:\Users\Administrator\Desktop\file1.pdf
